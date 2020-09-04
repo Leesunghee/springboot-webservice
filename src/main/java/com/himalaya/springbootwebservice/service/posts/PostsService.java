386693807c6e2ce9d81f33a6724a9f296a617b43
E590050 <realwunz@hyundai-autoever.com>
2020-09-04 19:20:56 +0900

    add Posts controller and service

---
+++ src/main/java/com/himalaya/springbootwebservice/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.himalaya.springbootwebservice.service.posts;
+
+import com.himalaya.springbootwebservice.domain.posts.PostsRepository;
+import com.himalaya.springbootwebservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
